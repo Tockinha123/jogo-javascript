e98eb1d99191f28a3b51baff467a189c00b12978
Tocka <150646534+Tockinha123@users.noreply.github.com>
2023-12-27 22:40:14 -0300

    Adicionada a função game e também adicionado contadores para uma melhor experiência com usuário

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerCount = 0; 
+let playerCount = 0; 
+
 function getComputerChoiche (){
     let myOptions = ["Rock", "Paper", "Scissors"];
 
@@ -18,22 +21,49 @@ function singleRound (computerSelection, playerSelection){
         return singleRound(getComputerChoiche(), getPlayerChoice());
     }
     else if (((computerSelection.localeCompare("Rock","en",{sensitivity:"accent"}) === 0) && (playerSelection.localeCompare("Scissors","en",{sensitivity:"accent"}) === 0))){
+        computerCount++;
         return console.log("You Lose! Rock beats Scissors");
     }
     else if (((computerSelection.localeCompare("Paper","en",{sensitivity:"accent"}) === 0) && (playerSelection.localeCompare("Rock","en",{sensitivity:"accent"}) === 0))){
+        computerCount++;
         return console.log("You Lose! Paper beats Rock");
     }
     else if (((computerSelection.localeCompare("Scissors","en",{sensitivity:"accent"}) === 0) && (playerSelection.localeCompare("Paper","en",{sensitivity:"accent"}) === 0))){
+        computerCount++;
         return console.log("You Lose! Scissors beats Paper");
     }
     else {
+        playerCount++; 
         return console.log("You Won !! Congratulations :D");
     }
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoiche();
+function game(){
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoiche();
 
-console.log(`Your choice: ${playerSelection}`);
-console.log(`Computer choice: ${computerSelection}`);
-console.log(singleRound(computerSelection, playerSelection));
+        console.log(`Your choice: ${playerSelection}`);
+        console.log(`Computer choice: ${computerSelection}`);
+        singleRound(computerSelection, playerSelection);
+        console.log("\n");
+        
+    }
+    if (playerCount >= 3) {
+        console.log("You Won the Best-of-Five !!!");
+        console.log(`Your score: ${playerCount}`);
+        console.log(`Computer score: ${computerCount}`);
+    }
+    else {
+        console.log("You Lose the Best-of-Five :( ");
+        console.log(`Your score: ${playerCount}`);
+        console.log(`Computer score: ${computerCount}`);
+    }
+}
+
+game();
+
+
+
+
+
